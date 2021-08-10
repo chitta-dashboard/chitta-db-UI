f9deba8be19c21f7a1e4fc75fd1235f6d59da474
dhanraj-web <dhanraj@aequalisys.com>
2021-08-10 10:53:59 +0530

    Waving hand icon update in dashboard

--- src/components/view/Dashboard/index.js
+++ src/components/view/Dashboard/index.js
@@ -10,9 +10,122 @@ import {
 } from "@material-ui/core";
 import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
 import CloseIcon from "@material-ui/icons/Close";
+import { makeStyles } from "@material-ui/core/styles";
 import { Chart } from "react-charts";
 import { NotificationSubCardData } from "../../../constants";
-import { useStyles } from "../../../assets/styles";
+import WavingHand from "../../../assets/images/wavingHand.svg";
+
+const useStyles = makeStyles((theme) => ({
+  dashboard_root: {
+    flexGrow: 1,
+  },
+  dashboard_header: {
+    height: "10vh",
+    padding: "1rem 0",
+  },
+  dashboard_TitleContainer: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "center",
+  },
+  dashboard_AdminBtnContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  dashboard_AdminBtn: {
+    color: "white",
+    background: "#36574C",
+    fontSize: "0.8rem",
+    textTransform: "none",
+  },
+  dashboard_AlertContainer: {
+    background: "#36574C",
+    display: "flex",
+    alignItems: "center",
+    color: "white",
+    padding: "0.8rem 1rem",
+    justifyContent: "space-between",
+  },
+  dashboard_alertText: {
+    display: "flex",
+    alignItems: "center",
+  },
+  dashboard_topbarContainer: {
+    height: "20vh",
+    display: "flex",
+    alignItems: "center",
+  },
+  dashboard_nameContainer: {
+    padding: "1rem",
+    "&:before": {
+      width: "20px",
+      height: "10px",
+      background: "gray",
+      position: "absolute",
+      right: 0,
+    },
+  },
+  dashboard_welcomeContainer: {
+    padding: "1rem",
+    display: "flex",
+    alignItems: "center",
+  },
+  dashboard_wavingIcon: {
+    height: "40px",
+    width: "40px",
+    marginRight: "0.5rem",
+  },
+
+  dashboard_NotificationAndSummaryContainer: {
+    display: "grid",
+    gridTemplateColumns: "55% 45%",
+  },
+  dashboard_summaryContainer: {},
+  dashboard_notificationSummaryTitle: {
+    margin: "0.5rem 0",
+  },
+  dashboard_notificationCardTitle: {
+    margin: "0.5rem 0",
+    display: "flex",
+    alignItems: "center",
+  },
+  dashboard_NotificationContainer: {},
+  dashboard_summaryGraphContainer: {
+    minHeight: "50vh",
+    marginRight: "1rem",
+    borderRadius: "10px",
+  },
+  dashboard_notificationCardContainer: {
+    maxHeight: "50vh",
+    minHeight: "50vh",
+    borderRadius: "10px",
+    overflow: "scroll",
+  },
+  dashboard_notificationSubCard: {
+    display: "grid",
+    padding: "1rem",
+    gridTemplateColumns: "10% 70% 20%",
+    fontSize: "0.8rem",
+  },
+  dashboard_graphSubContainer: {
+    width: "500px",
+    height: "250px",
+  },
+  alertLogo: {
+    height: "0.9rem",
+    marginBottom: "0.1rem",
+  },
+  notificationLogo: {
+    height: "1rem",
+    marginBottom: "0.1rem",
+  },
+  dashboard_closeIcon: {
+    height: "0.9rem",
+    cursor: "pointer",
+  },
+}));
 
 const Dashboard = () => {
   const classes = useStyles();
@@ -112,9 +225,16 @@ const Dashboard = () => {
       )}
 
       <Grid container className={classes.dashboard_topbarContainer}>
-        <Grid item xs={3} className={classes.dashboard_nameContainer}>
-          <Typography>HI,Mikeal</Typography>
-          <Typography>Here's Your current stats</Typography>
+        <Grid item xs={3} className={classes.dashboard_welcomeContainer}>
+          <img
+            src={WavingHand}
+            className={classes.dashboard_wavingIcon}
+            alt="wavinghand Logo"
+          />
+          <div>
+            <Typography>HI,Mikeal</Typography>
+            <Typography>Here's Your current stats</Typography>
+          </div>
         </Grid>
         <Grid item xs={2} className={classes.dashboard_nameContainer}>
           <Typography>Org.Registered</Typography>
