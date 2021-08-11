66b9aa837905f712f7c3935bb6a35d76a3c92d41
aeq-vinothkumar <vinoth@unilend.finance>
2021-08-11 10:44:01 +0530

    code cleanUP

--- src/components/view/UserLogin/UserLogin.js
+++ src/components/view/UserLogin/UserLogin.js
@@ -1,43 +1,20 @@
-import React, { useContext, useRef, useState, useEffect } from "react";
+import React, { useContext, useRef } from "react";
 import { useStyles } from "../../../assets/styles";
 import Logo from "../../../assets/images/nerkathir_logo.png";
 import { UserLoginContext } from "../../context/UserLoginContext";
 import { checkCeo, checkFarmer } from "../../../constants/config";
 
 export default function UserLogin() {
-  // const [userValue, setUserValue] = useState({});
-  const [phoneNumberCheck, setPhoneNumberCheck] = useState("");
-  const [passwordCheck, setPasswordCheck] = useState("");
   const { loginHandler, loginTypeHandler } = useContext(UserLoginContext);
   const phoneNumberRef = useRef("");
   const passwordRef = useRef("");
   const classes = useStyles();
 
-  // useEffect(() => {
-  //   console.log(
-  //     "check",
-  //     userValue[0]?.phoneNumber.toString().substring(0, 4) +
-  //       userValue[0]?.DOB.split("-").join("")
-  //   );
-  //   if (
-  //     userValue[0]?.phoneNumber.toString().substring(0, 4) +
-  //       userValue[0]?.DOB.split("-").join("") ===
-  //     passwordRef.current.value
-  //   ) {
-  //     loginHandler();
-  //   } else {
-  //     console.log("error");
-  //   }
-  // }, [userValue]);
-
   const submitHandler = (e) => {
     e.preventDefault();
-    // console.log("input", phoneNumberRef.current.value);
-    // console.log("input", passwordRef.current.value);
+
     checkCeo(phoneNumberRef.current.value)
       .then((res) => {
-        // console.log("ceo", res);
-        // setUserValue(res);
         if (
           res[0]?.phoneNumber.toString().substring(0, 4) +
             res[0]?.DOB.split("-").join("") ===
@@ -46,7 +23,7 @@ export default function UserLogin() {
           loginHandler();
           loginTypeHandler("Administrator");
         }
-        if (res.length == 0) {
+        if (res.length === 0) {
           checkFarmer(phoneNumberRef.current.value)
             .then((res) => {
               if (
