931a1cd15f6bd5c2a471a775dc9d519751de62c0
aeq-vinothkumar <vinoth@unilend.finance>
2021-08-12 12:26:05 +0530

    Decision page routing implemented

---
+++ src/components/view/Decision/Decision.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useStyles } from "../../../assets/styles";
+// import {
+//   Timeline,
+//   Content,
+//   ContentYear,
+//   ContentBody,
+//   Description,
+// } from "vertical-timeline-component-react";
+const Decision = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.decision}>
+      <h1>Decision</h1>
+    </div>
+  );
+};
+export default Decision;
+
+// const Decision = () => {
+//   return (
+//     <Timeline>
+//       <Content>
+//         <ContentYear
+//           startMonth="12"
+//           monthType="text"
+//           startDay="24"
+//           startYear="2016"
+//           currentYear
+//         />
+//         <ContentBody title="Amazing Title">
+//           <Description
+//             text="I'm an amazing event"
+//             optional="I'm an amazing optional text"
+//           />
+//           <Description
+//             text="I'm an amazing event"
+//             optional="I'm another amazing optional text"
+//           />
+//           <Description text="I'm an amazing event" />
+//         </ContentBody>
+//       </Content>
+//       {/* <Content>...</Content> */}
+//     </Timeline>
+//   );
+// };
+// export default Decision;
