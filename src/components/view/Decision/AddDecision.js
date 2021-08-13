8ab51de2c3cd9e6de7e47266667a7eff4979bd58
aeq-vinothkumar <vinoth@unilend.finance>
2021-08-13 12:32:42 +0530

    decision page submission implementation

--- src/components/view/Decision/AddDecision.js
+++ src/components/view/Decision/AddDecision.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Grid } from "@material-ui/core";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { useStyles } from "../../../assets/styles";
 import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 
 export default function AddDecision() {
+  const history = useHistory();
+  const dateRef = useRef("");
+  const decisionRef = useRef("");
   const classes = useStyles();
+  const formSubmission = (e) => {
+    e.preventDefault();
+    const params = {
+      date: dateRef.current.value,
+      decision: decisionRef.current.value,
+    };
+    history.goBack();
+    // console.log(params);
+  };
   return (
     <div className={classes.form}>
-      <form>
+      <form onSubmit={formSubmission}>
         <Grid className={classes.form_container} container spacing={3}>
           <Grid className={classes.adddetails_header} item xs={12}>
             <Link to="/decision" style={{ textDecoration: "none" }}>
@@ -34,6 +47,7 @@ export default function AddDecision() {
                 type="date"
                 placeholder="தேதி"
                 autoComplete="off"
+                ref={dateRef}
               />
             </Grid>
             <Grid className={classes.forminput_container} item xs={12}>
@@ -45,6 +59,7 @@ export default function AddDecision() {
                 type="text"
                 autoComplete="off"
                 style={{ padding: "15px", height: "auto" }}
+                ref={decisionRef}
               />
             </Grid>
             <Grid className={classes.forminput_container_btn} container>
