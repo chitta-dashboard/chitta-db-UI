2c1ccd51a87bbe5cbdfd7deb1a82cde4260e0cce
aeq-vinothkumar <vinoth@unilend.finance>
2021-08-13 11:59:50 +0530

    decision page implemented

---
+++ src/components/view/Decision/AddDecision.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { useStyles } from "../../../assets/styles";
+import { Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default function AddDecision() {
+  const classes = useStyles();
+  return (
+    <div className={classes.form}>
+      <form>
+        <Grid className={classes.form_container} container spacing={3}>
+          <Grid className={classes.adddetails_header} item xs={12}>
+            <Link to="/decision" style={{ textDecoration: "none" }}>
+              <Typography
+                variant="h5"
+                className={classes.addDetailbtn_container}
+                style={{ textDecoration: "none" }}
+              >
+                <ChevronLeftIcon className={classes.iconbtn} />
+                Add Decision
+              </Typography>
+            </Link>
+          </Grid>
+          <Grid
+            className={classes.forminput_containerrow}
+            container
+            spacing={3}
+          >
+            <Grid item xs={12}>
+              <input
+                className="farmer-input"
+                type="date"
+                placeholder="தேதி"
+                autoComplete="off"
+              />
+            </Grid>
+            <Grid className={classes.forminput_container} item xs={12}>
+              <textarea
+                className="farmer-input tamil"
+                placeholder="தீர்மானம்"
+                rows="8"
+                cols="50"
+                type="text"
+                autoComplete="off"
+                style={{ padding: "15px", height: "auto" }}
+              />
+            </Grid>
+            <Grid className={classes.forminput_container_btn} container>
+              <button
+                type="submit"
+                className={classes.submit_btn}
+                //   onClick={postCeoData}
+              >
+                SUBMIT
+              </button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+}
