8d9fbef9dca06658553cc1e42aaae3a3d53d2827
aeq-vinothkumar <vinoth@unilend.finance>
2021-08-17 16:06:03 +0530

    added react dev tools

--- src/App.js
+++ src/App.js
@@ -11,33 +11,32 @@ import UserLogin from "./components/view/UserLogin/UserLogin";
 import { UserLoginContext } from "./components/context/UserLoginContext";
 import { useContext, useEffect } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Cookies from "js-cookie";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+// import Cookies from "js-cookie";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const App = () => {
   const { isAuthenticated } = useContext(UserLoginContext);
 
   return (
     <QueryClientProvider client={queryClient}>
-    <div>
-      {isAuthenticated ? (
-        <Layout />
-      ) : (
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={UserLogin} />
-            <Redirect to="/" />
-          </Switch>
-        </BrowserRouter>
-      )}
-      {/* <Footer /> */}
-      <ToastContainer />
-    </div>
+      <div>
+        {isAuthenticated ? (
+          <Layout />
+        ) : (
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={UserLogin} />
+              <Redirect to="/" />
+            </Switch>
+          </BrowserRouter>
+        )}
+        {/* <Footer /> */}
+        <ToastContainer />
+      </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 };
